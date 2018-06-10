826fb0631251579e77dbfa3939ed74f318a5d712
playergoodm <playergood0@live.com>
2018-06-10 14:12:20 +0430

    This site now is useable. Template fixed and now they can use models.

--- Jobs/models.py
+++ Jobs/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
 class Job(models.Model):
     image = models.ImageField(upload_to='image/')
     summry = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.summry[:20]
--- Jobs/views.py
+++ Jobs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Job
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'Jobs/index.html')
+    my_job = Job.objects
+    return render(request, 'Jobs/index.html', {'jobs': my_job})
+
